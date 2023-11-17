b59259b0fea5ec9818c387afd62a7d110de14032
Ana Julia <anajuliadefendi@gmail.com>
2023-11-17 11:40:26 -0300

    e

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let menuMobile = document.querySelector('.menu_mobile')
+let imgMenu = document.querySelector('#imgMenu')
 
 function menu(){
-    menuMobile.style.left = '-100%'
-}
 
-function img(){
-    
+    if(menuMobile.style.left == '-100%'){
+        menuMobile.style.left = '0%';
+        imgMenu.src = "./imagens/close.svg"
+    }
+    else{
+        menuMobile.style.left = '-100%'
+        imgMenu.src = "./imagens/menu.svg"
+    }
 }
